933e21a2972c8dcb0a6836389e90a78acf2523eb
Reza Firdaus <azerus98@gmail.com>
2022-12-11 07:51:05 +0700

    add status messages

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_12_08_103718) do
+ActiveRecord::Schema.define(version: 2022_12_11_004922) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 2022_12_08_103718) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "receiver_id"
+    t.integer "status"
     t.index ["user_id"], name: "index_messages_on_user_id"
   end
 
