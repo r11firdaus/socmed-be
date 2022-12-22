84e1800cbd0ec9f7a950c7ff1b51140a2a9c24e3
Reza Firdaus <azerus98@gmail.com>
2022-12-22 10:34:49 +0700

    add unsubscribe command

--- app/channels/comments_channel.rb
+++ app/channels/comments_channel.rb
@@ -1,10 +1,11 @@
 class CommentsChannel < ApplicationCable::Channel
   def subscribed
-    stop_all_streams
-    stream_from "comment_channel"
+    # stop_all_streams
+    stream_from "comment_channel_for_#{params[:post_id]}"
   end
 
   def unsubscribed
     # Any cleanup needed when channel is unsubscribed
+    stop_stream_from "comment_channel_for_#{params[:post_id]}"
   end
 end
