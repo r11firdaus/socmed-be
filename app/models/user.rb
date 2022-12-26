e0dae1aeaf0d8b334fc374db14025fdd6ea61351
Reza Firdaus <azerus98@gmail.com>
2022-12-26 09:46:45 +0700

    fix model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,6 @@ class User < ApplicationRecord
   has_secure_password
   validates :email, presence: true, uniqueness: true
   validates :password_digest, presence: true
-  has_many :chats
+  has_many :messages
+  has_many :posts
 end
