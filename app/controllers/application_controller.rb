c5c8a84062f14740a5a5129db525e65eb81c1987
Reza Firdaus <azerus98@gmail.com>
2023-01-05 17:58:20 +0700

    fix user not called

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   skip_before_action :verify_authenticity_token
   def check_user(user_id)
-    @user = User.find(user_id)
-    @user.token == request.headers["HTTP_TOKEN"]
+    @current_user = User.find(user_id)
+    @current_user.token == request.headers["HTTP_TOKEN"]
   end
 end
